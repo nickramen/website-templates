57064c3542f772ff44a180037ba4635a0bad071d
nickramen <katherine.nicoleram@gmail.com>
2023-02-24 23:13:54 -0600

    finish design, responsive

--- developer/assets/js/main.js
+++ developer/assets/js/main.js
@@ -9,6 +9,16 @@ function opentab(tabname){
         tabcontent.classList.remove("active-tab")
     }
 
-    event.currentTarget.classList.add("active-linki")
+    event.currentTarget.classList.add("active-link")
     document.getElementById(tabname).classList.add("active-tab")
+}
+
+
+var sidemenu = document.getElementById("sidemenu");
+
+function openmenu(){
+    sidemenu.style.right = "0"
+}
+function closemenu(){
+    sidemenu.style.right = "-200px"
 }
